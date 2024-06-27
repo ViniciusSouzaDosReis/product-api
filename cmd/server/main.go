d9828bcc9f6ba72873ee5ff1964cd82669a34511
Vinicius Reis <vinireis010305@gmail.com>
2024-06-27 19:32:24 -0300

    feat: :sparkles: add rote to create user

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/ViniciusSouzaDosReis/product-api/configs"
 	"github.com/ViniciusSouzaDosReis/product-api/internal/entity"
 	"github.com/ViniciusSouzaDosReis/product-api/internal/infra/database/product_database"
+	"github.com/ViniciusSouzaDosReis/product-api/internal/infra/database/user_database"
 	"github.com/ViniciusSouzaDosReis/product-api/internal/webserver/handlers"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
@@ -20,15 +21,21 @@ func main() {
 	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
 	checkErr(err)
 	db.AutoMigrate(&entity.User{}, &entity.Product{})
+
 	productHandler := handlers.NewProductHandler(product_database.NewProductDB(db))
+	userHandler := handlers.NewUserHandler(user_database.NewUserDB(db))
 
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
+
 	r.Post("/product", productHandler.CreateProduct)
 	r.Get("/product", productHandler.GetProducts)
 	r.Get("/product/{id}", productHandler.GetProductById)
 	r.Put("/product/{id}", productHandler.UpdateProduct)
 	r.Delete("/product/{id}", productHandler.DeleteProduct)
+
+	r.Post("/user", userHandler.CreateUser)
+
 	http.ListenAndServe(":8080", r)
 }
 
---
+++ internal/webserver/handlers/user_handler.go
@@ -0,0 +1,40 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/ViniciusSouzaDosReis/product-api/internal/dto"
+	"github.com/ViniciusSouzaDosReis/product-api/internal/entity"
+	"github.com/ViniciusSouzaDosReis/product-api/internal/infra/database/interfaces"
+)
+
+type UserHandler struct {
+	UserDB interfaces.UserInterface
+}
+
+func NewUserHandler(db interfaces.UserInterface) *UserHandler {
+	return &UserHandler{
+		UserDB: db,
+	}
+}
+
+func (h *UserHandler) CreateUser(w http.ResponseWriter, r *http.Request) {
+	var user dto.CreateUserProduct
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	u, err := entity.NewUser(user.Name, user.Email, user.Password)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	err = h.UserDB.Create(u)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
+}
