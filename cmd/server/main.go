3109a4089ba5ffc04be0657fe9d994adc43ab92f
Vinicius Reis <vinireis010305@gmail.com>
2024-06-28 17:34:26 -0300

    feat: :sparkles: init docs app with swagger

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/ViniciusSouzaDosReis/product-api/configs"
+	_ "github.com/ViniciusSouzaDosReis/product-api/docs"
 	"github.com/ViniciusSouzaDosReis/product-api/internal/entity"
 	"github.com/ViniciusSouzaDosReis/product-api/internal/infra/database/product_database"
 	"github.com/ViniciusSouzaDosReis/product-api/internal/infra/database/user_database"
@@ -11,9 +12,28 @@ import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/jwtauth"
+	httpSwagger "github.com/swaggo/http-swagger"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
+
+// @title Go Expert API Example
+// @version 1.0
+// @description Product API with authentication
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name API Support
+// @contact.url http://www.swagger.io/support
+// @contact.email support@swagger.io
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host localhost:8080
+// @BasePath /
+// @securityDefinitions.apikey ApiKeyAuth
+// @in header
+// @name Authorization
 
 func main() {
 	configs, err := configs.LoadConfig(".")
@@ -40,6 +60,8 @@ func main() {
 
 	r.Post("/user", userHandler.CreateUser)
 	r.Post("/user/generate_token", userHandler.GenerateToken)
+
+	r.Get("/docs/*", httpSwagger.Handler(httpSwagger.URL("http://localhost:8080/docs/doc.json")))
 
 	http.ListenAndServe(":8080", r)
 }
